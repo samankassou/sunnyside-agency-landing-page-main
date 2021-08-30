b4916a49a868b068f70a36e382fd891f4017eeb9
samankassou <samankassoufoulla@gmail.com>
2021-08-30 09:09:45 +0100

    Make the triangle in the nav menu

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // grab elements
 const toggleBtn = document.getElementById('toggle-menu-btn')
 const navMenu = document.querySelector('.nav-links')
-
+const navLinks = document.querySelectorAll('.nav-links__item')
 // add event listeners
 toggleBtn.addEventListener('click', ()=>{
     navMenu.classList.toggle('menu-active')
+})
+navLinks.forEach((link) =>{
+    link.addEventListener('click', ()=>{
+        navMenu.classList.remove('menu-active')
+    })
 })
